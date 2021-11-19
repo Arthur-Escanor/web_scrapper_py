38f4992d03201f3947abac93660e2f11be1df5ea
Arthur-Escanor <64905903+Arthur-Escanor@users.noreply.github.com>
2021-11-18 18:36:53 -0600

    se termina el trabajo

--- main.py
+++ main.py
@@ -1,55 +1,59 @@
 import argparse
+import csv
+import datetime    
 import logging
+from os import write
 logging.basicConfig(level=logging.INFO)
 import re
-import news_page_objects as news 
-from common import config
 
 from requests.exceptions import HTTPError
 from urllib3.exceptions import MaxRetryError
 
-logger = logging.getLogger(__name__)
+import news_page_objects as news 
+from common import config
 
+
+logger = logging.getLogger(__name__)
 is_well_formed_link = re.compile(r'^https?://.+/.+$')
 is_root_path = re.compile(r'^/.+$')
-
 
 
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
 
-    logging.info(f'Beginning scrape for {host}')
+    logging.info('Beginning scrape for {}'.format(host))
     homepage = news.HomePage(news_site_uid, host)
 
     articles = []
     for link in homepage.article_links:
         #print(link)
         article = _fetch_article(news_site_uid, host, link)
-        
-        if articles:
+        if article:
             logger.info('Article fetched!!')
-            articles.append(articles)
-            print(articles.title)
-     
-    print(len(articles))        
+            articles.append(article)
+            break
+            
+    _save_articles(news_site_uid, articles) 
+                    
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(
+        news_site_uid=news_site_uid,
+        datetime=now)
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()    
+    with open(out_file_name, mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers) 
+        
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row) 
 
-    news_sites_choices = list(config()['news_sites'].keys())
-    parser.add_argument('news_site',
-                        help='The news site that you want to scrape',
-                        type=str,
-                        choices=news_sites_choices)
-
-    args = parser.parse_args()
-    _news_scraper(args.news_site)                    
-    
 def _fetch_article(news_site_uid, host, link):
-    logger.info(f'Start fetching article at {link}')
-    
-    article = None
-    
+    logger.info('Start fetching article at {}'.format(link))
+        
+    article = None    
     try:
         article = news.ArticlePage(news_site_uid, _build_link(host, link)) 
     except (HTTPError, MaxRetryError) as e:       
@@ -64,11 +68,19 @@ def _fetch_article(news_site_uid, host, link):
 def _build_link(host, link):
     if is_well_formed_link.match(link):
         return link
-    elif is_well_formed_link.match(link):
-        return link
     elif is_root_path.match(link):
-        return f'{host}{link}'
+        return '{}{}'.format(host, link)
     else:
-        return f'{host}/{link}'
-        
-    
+        return '{host}/{uri}'.format(host=host, uri=link)
+       
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()    
+
+    news_sites_choices = list(config()['news_sites'].keys())
+    parser.add_argument('news_site',
+                        help='The news site that you want to scrape',
+                        type=str,
+                        choices=news_sites_choices)
+
+    args = parser.parse_args()
+    _news_scraper(args.news_site)                    
--- news_page_objects.py
+++ news_page_objects.py
@@ -1,4 +1,3 @@
-from typing import Text
 import bs4
 import requests
 
